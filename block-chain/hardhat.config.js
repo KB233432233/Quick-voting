11b8c77da957dd2fce760b4f45007cb2ca966017
j-wadd <jawad@ASUS-TUF-F15.localdomain>
2026-04-06 23:51:17 +0300

    deployed succ on sepolia and added admin role, voteType and other stuff

--- block-chain/hardhat.config.js
+++ block-chain/hardhat.config.js
@@ -1,11 +1,19 @@
-// import viem from "@nomicfoundation/hardhat-viem";
+// // import viem from "@nomicfoundation/hardhat-viem";
 import ethers from "@nomicfoundation/hardhat-ethers";
 import "dotenv/config";
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-export default {
+import { defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
   plugins: [ethers],
   solidity: {
     version: "0.8.20",
@@ -17,20 +25,53 @@ export default {
       viaIR: true,
     },
   },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-  },
   networks: {
     hardhat: {
       type: "edr-simulated",
       chainType: "l1",
     },
-    // sepolia: {
-    //   url: SEPOLIA_RPC_URL,
-    //   accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
-    //   chainId: 11155111,
-    // }
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: SEPOLIA_RPC_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
   },
-};
+  test: {
+    solidity: {
+      timeout: 40000,
+    },
+  },
+});
+
+
+
+// export default {
+//   plugins: [ethers],
+//   solidity: {
+//     version: "0.8.20",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200,
+//       },
+//       viaIR: true,
+//     },
+//   },
+//   namedAccounts: {
+//     deployer: {
+//       default: 0,
+//     },
+//   },
+//   networks: {
+//     hardhat: {
+//       type: "edr-simulated",
+//       chainType: "l1",
+//     },
+//     // sepolia: {
+//     //   url: SEPOLIA_RPC_URL,
+//     //   accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+//     //   chainId: 11155111,
+//     // }
+//   },
+// };
